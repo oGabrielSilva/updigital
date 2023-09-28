9751645a147c153824b6e4904384651b71fcb41f
Kallyne <Kallyne.nascimento18@gmail.com>
2023-09-28 15:49:03 -0300

    mensagem

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,5 +4,9 @@ import { IndexController } from '../controllers/IndexController';
 const router = express.Router();
 
 router.get('/', IndexController.index);
+// router.post('/', (req, res) => {
+//     console.log(req.body)
+//     res.status(200).end()
+// });
 
 export default router;
