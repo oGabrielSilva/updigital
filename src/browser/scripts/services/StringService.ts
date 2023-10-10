74fb6ae9b7d717cf01052d0add958d3ef5049cd9
Smille <rawick2005@gmail.com>
2023-10-10 16:16:02 -0300

    mapeamento das strings

---
+++ src/browser/scripts/services/StringService.ts
@@ -0,0 +1,3 @@
+export class StringService {
+  private readonly dataString = document.querySelectorAll('[data-string]');
+}
--- src/resources/i18n/br.ts
+++ src/resources/i18n/br.ts
@@ -1,4 +1,8 @@
 export const br = {
   lockTitle: 'Aparelho bloqueado',
   registerPlaceholder: 'Matrícula do coordenador',
+  password: 'Senha',
+  unlockRequest: 'Realize o desbloqueio da máquina ao inserir as informações abaixo',
+  keepUnlocked: 'Manter desbloqueado',
+  unlock: 'Desbloquear',
 };
