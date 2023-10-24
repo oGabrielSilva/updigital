f2b7e24544a851e267a5fe1a8936d7a5bf9ee1a9
Smille <rawick2005@gmail.com>
2023-10-24 16:15:06 -0300

    tela de registro

--- src/resources/i18n/br.ts
+++ src/resources/i18n/br.ts
@@ -5,7 +5,7 @@ export const br = {
   unlockRequest: 'Realize o desbloqueio da máquina ao inserir as informações abaixo',
   keepUnlocked: 'Manter desbloqueado',
   unlock: 'Desbloquear',
-  titleRegister: "Tela de Registro do colaborador",
-  registerMessenger: "Mensagem de registro",
-
+  titleRegister: 'Tela de Registro do colaborador',
+  registerMessenger: 'insira suas informações abaixo:',
+  registerEmail: 'Insira seu E-mail',
 };
