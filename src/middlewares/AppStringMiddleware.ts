c08bd040655f9c1eab0c8bc117c63461b3a9eff9
Gabriel Silva <gabriel04gh1.gh@gmail.com>
2023-10-25 17:04:58 -0300

    strings

--- src/middlewares/AppStringMiddleware.ts
+++ src/middlewares/AppStringMiddleware.ts
@@ -1,9 +1,10 @@
 import { API_HEADER_KEY_LANG_NAME } from '../resources/constants/constants';
+import { br } from '../resources/i18n/br';
 
 export class AppStringMiddleware {
   public static resolve(req: Req, res: Res, next: Next): void {
-    const string = req.headers[API_HEADER_KEY_LANG_NAME];
-    console.log(string);
+    const stringCode = req.headers[API_HEADER_KEY_LANG_NAME];
+    res.locals.strings = stringCode === 'pt' ? br : br;
     next();
   }
 }
